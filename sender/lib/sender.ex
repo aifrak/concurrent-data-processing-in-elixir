6d70b8b27d2b3fee973db3d57429dac876b3d618
aifrak <aifrak@users.noreply.github.com>
2022-02-22 10:35:37 +0000

    Add code from "Starting Processes"

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -23,6 +23,10 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    Enum.each(emails, &send_email/1)
+    Enum.each(emails, fn email ->
+      Task.start(fn ->
+        send_email(email)
+      end)
+    end)
   end
 end
