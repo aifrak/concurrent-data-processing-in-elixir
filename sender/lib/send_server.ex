7dc890ae5b12ded32959d97579fdb6c8887b0705
aifrak <aifrak@users.noreply.github.com>
2022-02-23 10:28:42 +0000

    Add code from "Initializing the Process"

--- sender/lib/send_server.ex
+++ sender/lib/send_server.ex
@@ -1,3 +1,10 @@
 defmodule SendServer do
   use GenServer
+
+  def init(args) do
+    IO.puts("Received arguments: #{inspect(args)}")
+    max_retries = Keyword.get(args, :max_retries, 5)
+    state = %{emails: [], max_retries: max_retries}
+    {:ok, state}
+  end
 end
