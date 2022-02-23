d394cf3cc9fb464cabbefc5fa43a004b169075d1
aifrak <aifrak@users.noreply.github.com>
2022-02-23 10:42:13 +0000

    Add code from "Sending Process Messages"

--- sender/lib/send_server.ex
+++ sender/lib/send_server.ex
@@ -7,4 +7,15 @@ defmodule SendServer do
     state = %{emails: [], max_retries: max_retries}
     {:ok, state}
   end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:send, email}, state) do
+    Sender.send_email(email)
+    emails = [%{email: email, status: "sent", retries: 0}] ++ state.emails
+
+    {:noreply, %{state | emails: emails}}
+  end
 end
