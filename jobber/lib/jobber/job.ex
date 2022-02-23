0ad3615ad824d149ff98d9c5ab430dfebbc068b9
aifrak <aifrak@users.noreply.github.com>
2022-02-23 17:59:31 +0000

    Add code from "Performing Work"

--- jobber/lib/jobber/job.ex
+++ jobber/lib/jobber/job.ex
@@ -13,7 +13,45 @@ defmodule Jobber.Job do
     {:ok, state, {:continue, :run}}
   end
 
+  def handle_continue(:run, state) do
+    new_state = state.work.() |> handle_job_result(state)
+
+    if new_state.status == "errored" do
+      Process.send_after(self(), :retry, 5000)
+      {:noreply, new_state}
+    else
+      Logger.info("Job exiting #{state.id}")
+      {:stop, :normal, new_state}
+    end
+  end
+
+  def handle_info(:retry, state) do
+    # Delegate work to the `handle_continue/2` callback.
+    {:noreply, state, {:continue, :run}}
+  end
+
   defp random_job_id() do
     :crypto.strong_rand_bytes(5) |> Base.url_encode64(padding: false)
+  end
+
+  defp handle_job_result({:ok, _data}, state) do
+    Logger.info("Job completed #{state.id}")
+    %Jobber.Job{state | status: "done"}
+  end
+
+  defp handle_job_result(:error, %{status: "new"} = state) do
+    Logger.warn("Job errored #{state.id}")
+    %Jobber.Job{state | status: "errored"}
+  end
+
+  defp handle_job_result(:error, %{status: "errored"} = state) do
+    Logger.warn("Job retry failed #{state.id}")
+    new_state = %Jobber.Job{state | retries: state.retries + 1}
+
+    if new_state.retries == state.max_retries do
+      %Jobber.Job{new_state | status: "failed"}
+    else
+      new_state
+    end
   end
 end
