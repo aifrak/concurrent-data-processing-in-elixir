4a200be395c9ce6146dcad2ac2bfe6a3944ebc24
aifrak <aifrak@users.noreply.github.com>
2022-02-25 10:47:17 +0000

    Add code from "Building Your Data-Processing Pipeline"

--- scraper/lib/scraper.ex
+++ scraper/lib/scraper.ex
@@ -15,4 +15,14 @@ defmodule Scraper do
   def hello do
     :world
   end
+
+  def work() do
+    # For simplicity, this function is
+    # just a placeholder and does not contain
+    # real scraping logic.
+    1..5
+    |> Enum.random()
+    |> :timer.seconds()
+    |> Process.sleep()
+  end
 end
