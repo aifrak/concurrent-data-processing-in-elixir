7f0d7a8dd7c106cc2d956785dcc9c42a3f8ca863
aifrak <aifrak@users.noreply.github.com>
2022-02-26 18:09:24 +0000

    Add code from "Getting Snapshots from Slow-Running or Infinite Flows"

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -23,8 +23,14 @@ defmodule Airports do
   end
 
   def open_airports() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
     airports_csv()
     |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
     |> Flow.from_enumerable()
     |> Flow.map(fn row ->
       [row] = CSV.parse_string(row, skip_headers: false)
@@ -37,9 +43,25 @@ defmodule Airports do
       }
     end)
     |> Flow.reject(&(&1.type == "closed"))
-    |> Flow.partition(key: {:key, :country})
+    |> Flow.partition(window: window, key: {:key, :country})
     |> Flow.group_by(& &1.country)
-    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      # Show progress in IEx, or use the data for something else.
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done ->
+          # we emit the final result as a list of tuples
+          {events, acc}
+
+        {:every, 1000} ->
+          # we do not emit anything, since we're not finished processing yet
+          {[], acc}
+      end
+    end)
     |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
     |> Enum.to_list()
     |> List.flatten()
