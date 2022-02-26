823c945c24d4db70b9096c8d9f0396742909f4f1
aifrak <aifrak@users.noreply.github.com>
2022-02-26 14:59:13 +0000

    Add code from "Improving Performance with Streams"

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -24,16 +24,17 @@ defmodule Airports do
 
   def open_airports() do
     airports_csv()
-    |> File.read!()
-    |> CSV.parse_string()
-    |> Enum.map(fn row ->
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row ->
       %{
-        id: Enum.at(row, 0),
-        type: Enum.at(row, 2),
-        name: Enum.at(row, 3),
-        country: Enum.at(row, 8)
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
       }
     end)
-    |> Enum.reject(&(&1.type == "closed"))
+    |> Stream.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
   end
 end
