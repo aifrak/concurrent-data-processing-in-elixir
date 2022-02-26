fac328d7cb4989ac2646ff2d7d680ce738e55a09
aifrak <aifrak@users.noreply.github.com>
2022-02-26 14:42:02 +0000

    Add code from "Creating Flows and Reading Files"

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -21,4 +21,19 @@ defmodule Airports do
   def airports_csv() do
     Application.app_dir(:airports, "/priv/airports.csv")
   end
+
+  def open_airports() do
+    airports_csv()
+    |> File.read!()
+    |> CSV.parse_string()
+    |> Enum.map(fn row ->
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Enum.reject(&(&1.type == "closed"))
+  end
 end
