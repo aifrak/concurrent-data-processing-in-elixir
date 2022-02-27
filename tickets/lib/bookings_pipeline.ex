b3860aba9a11373f61c875ec36edec65ddf033f7
aifrak <aifrak@users.noreply.github.com>
2022-02-27 16:37:34 +0000

    Add code from "Implementing prepare_messages/2"

--- tickets/lib/bookings_pipeline.ex
+++ tickets/lib/bookings_pipeline.ex
@@ -31,8 +31,35 @@ defmodule BookingsPipeline do
     Broadway.start_link(__MODULE__, options)
   end
 
+  def prepare_messages(messages, _context) do
+    # Parse data and convert to a map.
+    messages =
+      Enum.map(messages, fn message ->
+        Broadway.Message.update_data(message, fn data ->
+          [event, user_id] = String.split(data, ",")
+          %{event: event, user_id: user_id}
+        end)
+      end)
+
+    users = Tickets.users_by_ids(Enum.map(messages, & &1.data.user_id))
+
+    # Put users in messages.
+    Enum.map(messages, fn message ->
+      Broadway.Message.update_data(message, fn data ->
+        user = Enum.find(users, &(&1.id == data.user_id))
+        Map.put(data, :user, user)
+      end)
+    end)
+  end
+
   def handle_message(_processor, message, _context) do
-    # Add your business logic here...
+    %{data: %{event: event, user: user}} = message
+
+    # TODO: check for tickets availability.
+
+    Tickets.create_ticket(user, event)
+    Tickets.send_email(user)
+
     IO.inspect(message, label: "Message")
   end
 end
